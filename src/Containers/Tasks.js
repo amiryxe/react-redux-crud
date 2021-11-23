847b1119f482b4bceee59b610cbdd5ac6a40fd3d
Amir Salehi <nik.amirsa@gmail.com>
2021-11-23 08:27:44 +0330

    style: convert add form section to a modal face

--- src/Containers/Tasks.js
+++ src/Containers/Tasks.js
@@ -1,11 +1,13 @@
 import AddTask from "../components/Task/AddTask";
 import TaskList from "../components/Task/TaskList";
+import Header from "../components/Header";
 
 export default function Tasks() {
     return (
-        <div>
+        <>
+            <Header />
             <AddTask />
             <TaskList />
-        </div>
+        </>
     );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>React redux CRUD</h1>
+
+            <button className="btn--success">+ Add Task</button>
+        </header>
+    )
+}
