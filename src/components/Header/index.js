a287dcb1b3b198b560f9c8aa66b952fd53b9cf4c
Amir Salehi <nik.amirsa@gmail.com>
2021-11-23 08:37:31 +0330

    handle open and close add task modal by click on add button and more

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,16 @@
+import { useDispatch } from "react-redux";
+import { showAddTask } from "../../actions/taskActions";
+
 export default function Header() {
+    const dispatch = useDispatch();
+
     return (
         <header className="header">
             <h1>React redux CRUD</h1>
 
-            <button className="btn--success">+ Add Task</button>
+            <button
+                onClick={() => dispatch(showAddTask(true))}
+                className="btn--success">+ Add Task</button>
         </header>
     )
 }
--- src/components/Task/AddTask.js
+++ src/components/Task/AddTask.js
@@ -24,6 +24,7 @@ function AddTask() {
         }
 
         dispatch(addTask(data));
+        dispatch(showAddTask(false));
 
         e.target.reset();
     }
