3f70828f4bd19146c7de8718803e742c48fe08a8
Amir <nik.amirsa@gmail.com>
2021-11-26 14:44:20 +0330

    filter tasks

--- src/components/Task/TaskList.js
+++ src/components/Task/TaskList.js
@@ -1,16 +1,26 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import TaskItem from "./TaskItem";
 
 function TaskList() {
     const list = useSelector(state => state.tasks.taskList) || [];
+    const currentShowMode = useSelector(state => state.tasks.showMode);
 
+    // TODO: handle filter tasks by show mode
     return (
         <div className="tasks">
             {
                 list.length > 0 ?
-                    list.map(task => (
-                        <TaskItem key={task.id} className="tasks__item" data={task} />
-                    ))
+                    list.map(task => {
+                        if (currentShowMode === 'done') {
+                            if (task.isDone) {
+                                return <TaskItem key={task.id} className="tasks__item" data={task} />
+                            }
+                        } else {
+                            return <TaskItem key={task.id} className="tasks__item" data={task} />
+                        }
+                        return <TaskItem key={task.id} className="tasks__item" data={task} />
+                    })
                     : <p style={{
                         padding: '3rem',
                         textAlign: 'center',
