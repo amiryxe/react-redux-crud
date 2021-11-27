79731a65c0f1beb01b9278761eef3acb5aa668ec
Amir <nik.amirsa@gmail.com>
2021-11-27 21:16:13 +0330

    handle change show mode state

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,55 +1,74 @@
-import { ADD_TASK, DELETE_TASK, EDIT_TASK, IS_SHOW_ADD_TASK, IS_DONE_TASK } from "../actions/types";
+import {
+  ADD_TASK,
+  DELETE_TASK,
+  EDIT_TASK,
+  IS_SHOW_ADD_TASK,
+  IS_DONE_TASK,
+  SHOW_MODE,
+} from "../actions/types";
 
 const initialState = {
-    taskList: [{
-        id: 1,
-        title: 'Task 1',
-        date: '2020-01-01',
-        description: 'Task 1 description',
-        isDone: false
-    }],
-    isShowAddTask: false,
-    showMode: 'all'
+  taskList: [
+    {
+      id: 1,
+      title: "Task 1",
+      date: "2020-01-01",
+      description: "Task 1 description",
+      isDone: false,
+    },
+  ],
+  isShowAddTask: false,
+  showMode: "all",
 };
 
 export default function taskReducer(state = initialState, action) {
-    switch (action.type) {
-        case ADD_TASK:
-            return {
-                ...state,
-                taskList: [...state.taskList, action.payload],
-            };
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        taskList: [...state.taskList, action.payload],
+      };
 
-        case DELETE_TASK:
-            return {
-                ...state,
-                taskList: state.taskList.filter(task => task.id !== action.payload),
-            };
+    case DELETE_TASK:
+      return {
+        ...state,
+        taskList: state.taskList.filter((task) => task.id !== action.payload),
+      };
 
-        case EDIT_TASK:
-            return {
-                ...state,
-                taskList: state.taskList.map(task =>
-                    task.id === action.payload.id ? action.payload : task),
-            };
+    case EDIT_TASK:
+      return {
+        ...state,
+        taskList: state.taskList.map((task) =>
+          task.id === action.payload.id ? action.payload : task
+        ),
+      };
 
-        case IS_SHOW_ADD_TASK:
-            return {
-                ...state,
-                isShowAddTask: action.payload
-            };
+    case IS_SHOW_ADD_TASK:
+      return {
+        ...state,
+        isShowAddTask: action.payload,
+      };
 
-        case IS_DONE_TASK:
-            return {
-                ...state,
-                taskList: state.taskList.map(task =>
-                    task.id === action.payload ? {
-                        ...task,
-                        isDone: !task.isDone
-                    } : task),
-            };
+    case IS_DONE_TASK:
+      return {
+        ...state,
+        taskList: state.taskList.map((task) =>
+          task.id === action.payload
+            ? {
+                ...task,
+                isDone: !task.isDone,
+              }
+            : task
+        ),
+      };
 
-        default:
-            return state;
-    }
+    case SHOW_MODE:
+      return {
+        ...state,
+        showMode: action.payload,
+      };
+
+    default:
+      return state;
+  }
 }
